07220c394b34f4a699791ad93d54f445f102ad7d
Faid Arya Pamungkas <faid.arya.ft24@mail.umy.ac.id>
2025-06-26 15:50:12 +0700

    Step 7 : if the vallue in the data field of the new node is greater than that

--- Tree.cpp
+++ Tree.cpp
@@ -75,4 +75,13 @@ void insert(){
         //6b: exit
         return;
     }
-   
+    //Step 7 if the value in the data field of the new node is greater than that of the 
+    else if (x > parent->info){
+        //7a: make the right child of parent point to the new node
+        parent->rightchild = newNode;
+
+        //7b: Exit
+        return;
+    }
+}
+
